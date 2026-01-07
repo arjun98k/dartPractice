63d8e2e8eaed5455bbe2d71b79bde944ecc3a155
arjun98k <arjunkandekar14@gmail.com>
2026-01-08 00:55:37 +0530

    function in dart

--- function/dartFunction.dart
+++ function/dartFunction.dart
@@ -1,8 +1,17 @@
 void main(){
 var addition = add(12, 13);
 print(addition);  
+var givemecube = cube(20);
+print(givemecube);
+showMsg(name:"arjun",rollno: 98);
 }
 
-int add(int a , int b) {
+ add(int a , int b) {
  return a+b;
+}
+
+int cube(int y) => y*y*y;
+
+void showMsg({required String? name , int? rollno}){
+    print("hey my name is $name and my roll number is $rollno");
 }

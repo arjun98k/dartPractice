2d7c04b916c592bc4a2d1bfe2b9f2bc4935caafb
arjun98k <arjunkandekar14@gmail.com>
2026-02-04 12:31:21 +0530

    Oops inheritance

--- Oops5.dart
+++ Oops5.dart
@@ -1,16 +1,24 @@
 void main(List<String> args) {
-  Dogs dog = Dogs();
+  Animal dog = Dogs();
    dog.eat();
+   dog.sleep();
 }
 
 class Animal {
   void eat(){
     print("animal can eat food");
   }
+  void sleep(){
+    print("animal sleep 8 hr");
+  }
 }
 
 class Dogs extends Animal {
   void walk(){
     print("dog walk on 4 Leg");
+  }
+
+  void sleep(){
+    print("dog sleep 6 hr");
   }
 }
